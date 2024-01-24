96ee2217599e6d9af44306fea8c5642fdea426bb
Yunus Emre Taşdemir <emre.tasdemir@btc-ag.com.tr>
2024-01-24 15:07:27 +0300

    Setup hilt.

---
+++ app/src/main/java/com/yetsdmr/movieappcleanarchitecturecompose/di/AppModule.kt
@@ -0,0 +1,10 @@
+package com.yetsdmr.movieappcleanarchitecturecompose.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+}
