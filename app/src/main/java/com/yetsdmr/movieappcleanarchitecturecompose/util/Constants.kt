1a13d17c54d88127ba43e0c648ac5157bc68a493
Yunus Emre Taşdemir <emre.tasdemir@btc-ag.com.tr>
2024-01-24 16:11:45 +0300

    Added util package.

---
+++ app/src/main/java/com/yetsdmr/movieappcleanarchitecturecompose/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.yetsdmr.movieappcleanarchitecturecompose.util
+
+object Constants {
+    const val API_KEY = "f64ea126"
+    const val BASE_URL = "https://www.omdbapi.com/"
+}
---
+++ app/src/main/java/com/yetsdmr/movieappcleanarchitecturecompose/util/Result.kt
@@ -0,0 +1,9 @@
+package com.yetsdmr.movieappcleanarchitecturecompose.util
+
+sealed interface Result<out T> {
+    data object Idle: Result<Nothing>
+    data object Loading: Result<Nothing>
+
+    data class Success<out T>(val data: T): Result<T>
+    data class Error(val error: Throwable): Result<Nothing>
+}
